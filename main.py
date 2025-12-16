d07e309a8eae53d3b6cf8327b14a77acb2a24ec7
Marcos Lucas Pires Moraes Santos <marcoslucaspiresmoraessantos@gmail.com>
2025-12-16 11:24:19 -0300

    criando função de ler entrada

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 from random import randint
+import time
+import os
 
 def gerarNumero():
     n = randint(0, 10)
     return n
 
+def limparTerminal():
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+def lerNumero(msg):
+    while True:
+        entrada = input(msg)
+        try:
+            return float(entrada)
+        except ValueError:
+            print("Entrada inválida! Digite um número.")
+            time.sleep(1)
+            limparTerminal()
+
 continuar = True
+
+print("Bem-vindo ao jogo de adivinhação!")
 
 while(continuar):
     numeroEscolhido = gerarNumero()
-    print("Bem-vindo ao jogo de adivinhação!")
+    
     
     
