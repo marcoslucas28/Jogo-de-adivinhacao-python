5d01ff29ec66a0e37b14fd97624f034fbcc6c3c8
Marcos Lucas Pires Moraes Santos <marcoslucaspiresmoraessantos@gmail.com>
2025-12-16 13:47:17 -0300

    Finalizando jogo

--- main.py
+++ main.py
@@ -22,10 +22,44 @@ def lerNumero(msg):
 
 continuar = True
 
+arquivo = open("arquivo.txt", "a")
+arquivo = open("arquivo.txt", "r")
+
+acertos = arquivo.readline()
+
+if(acertos == ""): acertos = 0
+
+acertos = int(acertos)
+
 print("Bem-vindo ao jogo de adivinhação!")
 
 while(continuar):
     numeroEscolhido = gerarNumero()
+    print("Você tem", acertos, "acertos" )
+    tentativa = lerNumero("tente adivinhar o número: ")
+
+    if(tentativa == numeroEscolhido): 
+        acertos += 1
+        print("Parabéns, você acertou!")
+
+    if(tentativa != numeroEscolhido): print("Você errou")
+    while(1):
+        querContinuar = input("Você deseja tentar de novo?(s/n): ")
+        if querContinuar in ('s', 'S', 'n', 'N'):
+            limparTerminal()
+            break
+        print("Escolha uma opção valida")
+        time.sleep(2)
+        limparTerminal()
+
+    if querContinuar in ('s', 'S'): continuar = True
+    if querContinuar in ('n', 'N'): continuar = False
+    limparTerminal()
+
+arquivo = open("arquivo.txt", "a")   
+acertos = str(acertos)
+arquivo.write(acertos)
+print("Obrigado por jogar")
     
     
     
